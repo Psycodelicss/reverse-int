f3ccb5f34da0b48b4678fe58a7ef2ee8326b6d72
Psycodelicss <psycodelicss@mail.ru>
2023-01-05 11:53:06 +0500

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let i = 0, result = '', text = String(n);
+    
+    while (i < String(text).length) {
+        if (text[i] === '-') {
+            i += 1;
+        } else {
+            result = `${text[i]}${result}`;
+            i += 1;
+        }
+    }
+    return Number(result);
 }
